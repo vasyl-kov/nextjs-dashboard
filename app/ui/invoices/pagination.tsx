174e4d1c1e690eabea6239d7995019ce7fa5c807
vasyl-kov <vkoltsov316@gmail.com>
2024-01-18 16:38:30 +0200

    add pagination, search, adding customers

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,12 +4,22 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import {  usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: comment in this code when you get to this point in the course
 
   // const allPages = generatePagination(currentPage, totalPages);
+	const pathname = usePathname(),
+				searchParams = useSearchParams(),
+				currentPage = Number(searchParams.get('page')) || 1;
 
+
+	const createPageURL = (pageNumber: string | number) => {
+		const params = new URLSearchParams(searchParams);
+		params.set('page', pageNumber.toString());
+		return `${pathname}?${params.toString()}`;
+	}
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
